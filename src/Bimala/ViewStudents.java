36bd9442abb5316e52d24437acf4399b32687be9
gedilabimalabsc24-code <gedilabimalabsc24@rvu.edu.in>
2025-11-10 07:06:05 +0530

    Update ViewStudents.java
    Updated ViewStudents

--- src/Bimala/ViewStudents.java
+++ src/Bimala/ViewStudents.java
@@ -1,17 +1,39 @@
-package student.gui;
+/*
+ * ViewStudents.java - Displays list of all students
+ * Author: Bimala
+ * Date: November 2025
+ */
 
+package Bimala;
+
+import Dhruvi.Student;
+import Archana.StudentList;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class ViewStudents {
-    public void showView() {
-        VBox root = new VBox(10, new Label("List of Students (Demo view)"));
-        root.setStyle("-fx-padding: 20;");
+    private StudentList studentList = new StudentList();
+
+    public void show() {
         Stage stage = new Stage();
-        stage.setScene(new Scene(root, 300, 200));
         stage.setTitle("View Students");
+
+        ListView<String> listView = new ListView<>();
+
+        if (studentList.getStudents().isEmpty()) {
+            listView.getItems().add("No students available.");
+        } else {
+            for (Student s : studentList.getStudents()) {
+                listView.getItems().add(s.getName() + " (" + s.getStudentID() + ") - " + s.getMarks() + " marks");
+            }
+        }
+
+        VBox box = new VBox(10, listView);
+        box.setStyle("-fx-padding: 20;");
+
+        stage.setScene(new Scene(box, 400, 300));
         stage.show();
     }
 }
